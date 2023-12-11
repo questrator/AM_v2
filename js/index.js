16fb2f25f301a6925731db27177d605b640923b6
Slava Ivanov <drumtrack@ya.ru>
2023-12-11 12:51:54 +0300

    upd

--- js/index.js
+++ js/index.js
@@ -2,13 +2,22 @@ import Track from "./Track.js";
 
 const noiseKnob = document.querySelector(".noise");
 noiseKnob.addEventListener("mousedown", mousedownHandler);
+noiseKnob.addEventListener("touchstart", touchstartHandler);
 
 function mousedownHandler(e) {
-    console.log(e.target)
     rotateEase(e);
     noiseKnob.addEventListener("mousemove", rotate);
     noiseKnob.addEventListener("mouseup", () => {
         noiseKnob.removeEventListener("mousemove", rotate);
+    });
+}
+
+function touchstartHandler(e) {
+    console.log("tstart")
+    rotateEase(e);
+    noiseKnob.addEventListener("touchmove", rotate);
+    noiseKnob.addEventListener("touchend", () => {
+        noiseKnob.removeEventListener("touchmove", rotate);
     });
 }
 
@@ -19,9 +28,17 @@ function getAngle(e) {
     return 180 / Math.PI * Math.atan2(cY, cX) - 135;
 }
 
-function rotate(e) {    
+function getAngleTouch(e) {
+    const center = [e.touches[0].target.offsetHeight / 2, e.touches[0].target.offsetWidth / 2];
+    const position = [e.touches[0].clientX - e.touches[0].target.parentElement.parentElement.offsetLeft, e.touches[0].clientY - e.touches[0].target.parentElement.parentElement.offsetTop];
+    const [cX, cY] = center.map((e, i) => position[i] - e);
+    return 180 / Math.PI * Math.atan2(cY, cX) - 135;
+}
+
+function rotate(e) {  
+    console.log("rotate")  
     noiseKnob.style.transitionDuration = "0ms";
-    noiseKnob.style.transform = `rotate(${getAngle(e)}deg)`;
+    noiseKnob.style.transform = `rotate(${e.touches[0] ? getAngleTouch(e) : getAngle(e)}deg)`;
 }
 function rotateEase(e) {
     noiseKnob.style.transitionDuration = "300ms";
